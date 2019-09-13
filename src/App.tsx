83667f22e0026745a7b3207226d6d61c86dd8bec
kakakakakku <y.yoshida22@gmail.com>
2019-09-13 13:08:58 +0900

    Added List

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: '#fffde7',
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
 const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <List component="nav">
+        <ListItem>
+          <ListItemText primary="List 1" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="List 2 (button)" />
+        </ListItem>
+        <ListItem disabled>
+          <ListItemText primary="List 3 (disabled)" />
+        </ListItem>
+        <ListItem selected>
+          <ListItemText primary="List 4 (selected)" />
+        </ListItem>
+        <ListItem divider>
+          <ListItemText primary="List 5 (divider)" />
+        </ListItem>
+        <ListItemLink href="https://kakakakakku.hatenablog.com/">
+          <ListItemText primary="List 6 (link)" />
+        </ListItemLink>
+      </List>
     </div>
   );
 }
